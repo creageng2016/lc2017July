0d40f9714951af2e171ad225c346ffbc10e245eb
hgeng30 <creageng@gatech.edu>
2018-05-15 23:49:07 -0700

    241

---
+++ 241. Different Ways to Add Parentheses.py
@@ -0,0 +1,40 @@
+import re
+import operator
+
+class Solution(object):
+    def diffWaysToCompute(self, input):
+        """
+        :type input: str
+        :rtype: List[int]
+        """
+        # if len(input) == 0:
+        #     return 0
+        # if len(input) == 1:
+        #     return input[0]
+        patttern    = re.compile(r'\s+')
+        input       = re.sub(patttern, '', input) 
+
+        token   = re.split("(\D)", input)
+        # print token
+        nums    = map(int, token[::2])
+        # print nums
+        ops     = map( {"+":    operator.add, 
+                        "-":    operator.sub,
+                        "*":    operator.mul}.get,
+                    token[1:-1:2]
+            )
+        # print ops
+        def build(lo, hi):
+            if lo == hi:
+                return [nums[lo]]
+            return [ops[i](a, b) 
+                    for i in xrange(lo, hi)
+                    for a in build(lo,i)
+                    for b in build(i + 1, hi)
+                ]
+        return build(0, len(nums) - 1)
+
+
+if __name__ == '__main__':
+    input = "2  *3-4*5"
+    print Solution().diffWaysToCompute(input)
