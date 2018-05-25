2f1928a349dfd6fca1004760e0904ca28b7316ed
hgeng30 <creageng@gatech.edu>
2018-05-24 23:50:32 -0700

    299

--- 299. Bulls and Cows.py
+++ 299. Bulls and Cows.py
@@ -22,4 +22,9 @@ class Solution(object):
             bk += min(secret.count(_), guess.count(_))
         return str(ak)+"A"+str(bk)+"B"
         
-        
+
+class Solution(object):
+    def getHint(self, secret, guess):
+        bulls = sum(map(operator.eq, secret, guess))
+        both = sum(min(secret.count(x), guess.count(x)) for x in set(guess))
+        return '%dA%dB' % (bulls, both - bulls)
